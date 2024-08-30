44d6cd6dbcc43e09a5f9dbbfc921bf5ba799eb56
Herbenishd87 <herbenis.hernandez632@asys.edu.co>
2024-08-29 20:42:46 -0500

    NuevaCarpetaJavaScript

---
+++ Semana 5/1 Variables y Constantes/index.js
@@ -0,0 +1,22 @@
+console.log('Hola Mundo');
+console.table({"Hola": "Mundo"});
+console.table("Hola Mundo");
+const prueba = "Hola Mundo Prueba";
+console.log(prueba);
+
+const marcador = "Verde";
+const marcador2 = "Rojo"
+const marcador3 = "Negro"
+const marcadores = ["rojo","verde","negro","azul"]
+
+var variablevar= "esta es una variable var"
+let variablelet= "esta es una variable let"
+const variableconst= "esta es una variableconst"
+
+
+
+console.log(marcador)
+console.log(marcadores)
+console.log(variablevar)
+console.log(variablelet)
+console.log(variableconst)
---
+++ Semana 6/Comentarios/Index.js
@@ -0,0 +1,8 @@
+// Este es un comentario de una sola línea
+
+/* Este es un
+Comentario de
+Multiples Líneas
+*/
+
+
