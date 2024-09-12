883fb3ab4b650322ad4df324f543c0e2efb4bf24
Herbenishd87 <herbenis.hernandez632@asys.edu.co>
2024-09-12 08:27:01 -0500

    practicas semana7

---
+++ Semana 7/Concatenacion/index.js
@@ -0,0 +1,24 @@
+const cadena1 = "Hola ";
+const cadena2 = "Mundo";
+const cadena3= cadena1 + cadena2;
+
+console.log(cadena1 + cadena2);
+console.log (cadena3);
+
+const nombre = "Herbenis ";
+const mensaje = "cuentanos ";
+const saludo = "como estas";
+const imprimir = nombre +" " + mensaje +" " + saludo
+const imprimirsaltolinea = nombre + " \n "+ mensaje + " \n "+ saludo
+console.log(imprimirsaltolinea)
+
+
+const nombre2 = "Herbenis ";
+const mensaje2 = "cuentanos ";
+const saludo2 = "como estas";
+
+
+const imprimir2= `${nombre2} ${mensaje2} ${saludo2}`
+console.log(imprimir2)
+
+
